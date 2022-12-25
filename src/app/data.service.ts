0f70ec5731e5caa80a85200aad633ba8259d8437
AnandRajput-ASR <39948120+AnandRajput-ASR@users.noreply.github.com>
2022-12-25 23:32:48 +0530

    data.service updated

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,36 +3,38 @@ import { of } from 'rxjs';
 
 @Injectable()
 export class DataService {
-
-  constructor() { }
+  constructor() {}
 
   getSales() {
     return of({
-      "year1": {
-        "volumeSales": "1.09",
-        "valueSales": "1.23"
+      year1: {
+        volumeSales: '1.09',
+        valueSales: '1.23',
       },
-      "year2": {
-        "volumeSales": "0.11",
-        "valueSales": "1.56"
+      year2: {
+        volumeSales: '3.11',
+        valueSales: '1.56',
       },
-      "year3": {
-        "volumeSales": "0.12",
-        "valueSales": "1.69"
+      year3: {
+        volumeSales: '2.12',
+        valueSales: '4.69',
       },
-      "year4": {
-        "volumeSales": "0.12",
-        "valueSales": "1.64"
+      year4: {
+        volumeSales: '1.12',
+        valueSales: '1.64',
       },
-      "year5": {
-        "volumeSales": "0.10",
-        "valueSales": "1.41"
+      year5: {
+        volumeSales: '1.10',
+        valueSales: '2.41',
       },
-      "total": {
-        "volumeSales": "0.55",
-        "valueSales": "7.53"
-      }
+      Year6: {
+        volumeSales: '8.55',
+        valueSales: '7.53',
+      },
+      Year7: {
+        volumeSales: '1.55',
+        valueSales: '7.53',
+      },
     });
   }
-
 }
